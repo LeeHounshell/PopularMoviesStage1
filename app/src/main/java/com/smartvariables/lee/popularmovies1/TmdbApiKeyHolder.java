ea1acc761e44b82b0eb6a23ad8487336c721e5c6
Lee Hounshell <lee.hounshell@gmail.com>
2016-03-02 13:18:34 -0800

    API Key Holder

---
+++ app/src/main/java/com/smartvariables/lee/popularmovies1/TmdbApiKeyHolder.java
@@ -0,0 +1,9 @@
+package com.smartvariables.lee.popularmovies1;
+
+public class TmdbApiKeyHolder {
+    final private static String key = "<INSERT TMDB API KEY HERE>";
+
+    public static String getKey() {
+        return key;
+    }
+}
